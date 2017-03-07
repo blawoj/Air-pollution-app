e8ed7d419f405d9b0e079daa254a7dd5f1db7ade
blawoj <blazejwojciechowski92@gmail.com>
2017-03-07 14:06:16 +0100

    Add files via upload

---
+++ app/src/main/java/com/example/ja/airpollution/WeatherDataContext.java
@@ -0,0 +1,11 @@
+package com.example.ja.airpollution;
+
+public class WeatherDataContext {
+
+    /*Pozyskiwnaie danych przez API https://breezometer.com/api/ oraz wyciaganie danych z jsona.
+    Wyciągnięte dane przedstawiane są w postaci słupków, wykresów. Dodatkowo dostajemy inforamcje
+     czy warto w danym momencie wychodzić na zewnątrz, jakie są normy dla danych pierwiastków, jakie
+     są skutki wdychania takiego powietrza.
+     */
+
+}
